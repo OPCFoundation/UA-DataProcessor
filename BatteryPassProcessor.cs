f8689677806bdac179ec4c369c8429f6a6f3d201
Lisa Angold <lisa.angold@ffb.fraunhofer.de>
2026-04-15 12:57:34 +0100

    Altered the DPP creation function to query the iddmc of a battery and time produced, then gets eol data based on that

--- BatteryPassProcessor.cs
+++ BatteryPassProcessor.cs
@@ -26,48 +26,27 @@ namespace Opc.Ua.Data.Processor
 
         public void Process()
         {
-            // we have two production lines in the manufacturing ontologies production line simulation and they are connected like so:
-            // assembly -> test -> packaging
-            CalcPCFForProductionLine("Munich", "48.1375", "11.575", 6);
-            CalcPCFForProductionLine("Seattle", "47.609722", "-122.333056", 10);
+            GetDPPDataForProductionLine("muenster", 10);
         }
 
-        private void CalcPCFForProductionLine(string productionLineName, string latitude, string longitude, int idealCycleTime)
+       private void GetDPPDataForProductionLine(string productionLineName, int idealCycleTime)
         {
             try
             {
-                // first of all, retrieve carbon intensity for the location of the production line
-                CarbonIntensityQueryResult currentCarbonIntensity = WattTimeClient.GetCarbonIntensity(latitude, longitude).GetAwaiter().GetResult();
-                if ((currentCarbonIntensity != null) && (currentCarbonIntensity.data.Length > 0))
+                // get the latest idDmc of a cell (latest cell produced)
+                Dictionary<string, object> latestProductProducedSerialNumber = ADXQueryForLastKnownValue("production", productionLineName, "idDmc");
+
+                if ((latestProductProducedSerialNumber != null) && (latestProductProducedSerialNumber.Count > 0))
                 {
-                    // check if a new product was produced (last machine in the production line, i.e. packaging, is in state 2 ("done") with a passed QA)
-                    // and get the products serial number and energy consumption at that time
-                    Dictionary<string, object> latestProductProduced = ADXQueryForSpecificValue("packaging", productionLineName, "Status", 2);
-                    if ((latestProductProduced != null) && (latestProductProduced.Count > 0))
-                    {
-                        Dictionary<string, object> serialNumberResult = ADXQueryForSpecificTime("packaging", productionLineName, "ProductSerialNumber", ((DateTime)latestProductProduced["Timestamp"]).ToString("yyyy-MM-dd HH:mm:ss"), idealCycleTime);
-                        double serialNumber = (double)serialNumberResult["OPCUANodeValue"];
+                    
+                    // get the EOL data that are sent very close timewise to the cellID
+                    Dictionary<string, object> EOLData = ADXQueryForEOLData("production", productionLineName, ((DateTime)latestProductProducedSerialNumber["TelemetryTime"]).ToString("yyyy-MM-dd HH:mm:ss"), idealCycleTime);
+                    
+                    // persist in Cloud Library
+                    PersistInCloudLibrary(productionLineName, latestProductProducedSerialNumber)
 
-                        Dictionary<string, object> timeItWasProducedPackaging = ADXQueryForSpecificValue("packaging", productionLineName, "ProductSerialNumber", serialNumber);
-                        Dictionary<string, object> energyPackaging = ADXQueryForSpecificTime("packaging", productionLineName, "EnergyConsumption", ((DateTime)timeItWasProducedPackaging["Timestamp"]).ToString("yyyy-MM-dd HH:mm:ss"), idealCycleTime);
-
-                        // check each other machine for the time when the product with this serial number was in the machine and get its energy comsumption at that time
-                        Dictionary<string, object> timeItWasProducedTest = ADXQueryForSpecificValue("test", productionLineName, "ProductSerialNumber", serialNumber);
-                        Dictionary<string, object> energyTest = ADXQueryForSpecificTime("test", productionLineName, "EnergyConsumption", ((DateTime)timeItWasProducedTest["Timestamp"]).ToString("yyyy-MM-dd HH:mm:ss"), idealCycleTime);
-
-                        Dictionary<string, object> timeItWasProducedAssembly = ADXQueryForSpecificValue("assembly", productionLineName, "ProductSerialNumber", serialNumber);
-                        Dictionary<string, object> energyAssembly = ADXQueryForSpecificTime("assembly", productionLineName, "EnergyConsumption", ((DateTime)timeItWasProducedAssembly["Timestamp"]).ToString("yyyy-MM-dd HH:mm:ss"), idealCycleTime);
-
-                        // calculate the total energy consumption for the product by summing up all the machines' energy consumptions (in Ws), divide by 3600 to get seconds and multiply by the ideal cycle time (which is in seconds)
-                        double energyTotal = ((double)energyAssembly["OPCUANodeValue"] + (double)energyTest["OPCUANodeValue"] + (double)energyPackaging["OPCUANodeValue"]) / 3600 * idealCycleTime;
-
-                        // finally calculate the scope 2 product carbon footprint by multiplying the full energy consumption by the current carbon intensity
-                        float scope2Emissions = (float)energyTotal * currentCarbonIntensity.data[0].intensity.actual;
-
-                        // persist in Cloud Library
-                        PersistInCloudLibrary(productionLineName, serialNumber, scope2Emissions);
-                    }
                 }
+                // }
             }
             catch (Exception ex)
             {
@@ -75,32 +54,18 @@ namespace Opc.Ua.Data.Processor
             }
         }
 
-        private void PersistInCloudLibrary(string productionLineName, double serialNumber, float pcf)
+        private void PersistInCloudLibrary(string productionLineName, string serialNumber)
         {
             string dppName = "BatteryPassV6_" + productionLineName + "_" + serialNumber.ToString();
 
             // write the values to a JSON file
-            Dictionary<string, string> values = new() {
-                { "i=2", productionLineName + "_" + serialNumber.ToString() }, // UniqueProductIdentifier
-                { "i=3", "1.0" },                       // DppSchemaVersion
-                { "i=4", "Released" },                  // DppStatus
-                { "i=5", DateTime.UtcNow.ToString() },  // LastUpdate
-                { "i=6", "OPC Foundation" },            // EconomicOperatorId
-                { "i=11", "GHG Protocol" },             // PCFCalculationMethod
-                { "i=12", pcf.ToString() },             // PCFCO2eq
-                { "i=13", serialNumber.ToString() },    // PCFReferenceValueForCalculation
-                { "i=14", "gCO2" },                     // PCFQuantityOfMeasureForCalculation
-                { "i=15", "Production & Usage" },       // PCFLifeCyclePhase
-                { "i=16", "Scope 2 & 3 Emissions" },    // ExplanatoryStatement
-                { "i=21", productionLineName },         // PCFGoodsAddressHandover.CityTown
-                { "i=23", DateTime.UtcNow.ToString() }  // PublicationDate
-            };
+            Dictionary<string, string> values = new() {}
 
             UANameSpace nameSpace = new() {
                 Title = dppName,
                 License = "MIT",
                 CopyrightText = "OPC Foundation",
-                Description = "Sample PCF for Digital Twin Consortium production line simulation"
+                Description = "Sample BPP for OPCF / Fraunhofer FFB production line simulation"
             };
             nameSpace.Nodeset.NodesetXml = File.ReadAllText("./BatteryPassV6.NodeSet2.xml").Replace("BatteryPassV6", dppName);
             var url = QueryHelpers.AddQueryString(
@@ -125,7 +90,6 @@ namespace Opc.Ua.Data.Processor
                 Console.WriteLine("Successfully uploaded BatteryPass to Cloud Library for " + dppName);
             }
         }
-
        private float FindPcf(ErpNode node)
         {
             if (node.events != null)
